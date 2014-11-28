5174b5481cc64b9ed7b7cec256d06a0f17f16096
svetlana,lawrentyeva <svetlana,lawrentyeva@gmail.com>
2014-11-28 21:38:05 +0200

    init

---
+++ src/main/HelloWorld.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by therioyo on 28.11.14.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
